ebab4c20fd17fa7a36d50d9fd0decf5d4ba05e7a
Eddie <itein@Hotmail.com>
2017-08-23 22:55:54 -0700

    working app

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+//DATA
+
+
+var friends = [{
+    "name": "Bart",
+    "photo": "http://bartsimpsonpictures.squarelogic.net/bart-simpson-11.gif",
+    "scores" : [5,5,2,3,1,5,2,1,3,5]
+  }, {
+    "name": "Homer",
+    "photo": "https://pbs.twimg.com/profile_images/725013638411489280/4wx8EcIA.jpg",
+    "scores" : [5,3,2,3,4,5,1,1,3,5]
+  }, {
+    "name": "Marge",
+    "photo": "http://www.simpsoncrazy.com/content/pictures/marge/MargeSimpson5.gif",
+    "scores" : [5,3,1,3,4,3,1,2,3,5]
+  }, {
+    "name": "Lisa",
+    "photo": "https://i.pinimg.com/736x/23/10/37/23103793c616e7d8105ee74f53d143dd--framed-wall-art-framed-art-prints.jpg",
+    "scores" : [5,3,3,3,2,2,1,1,3,5]
+  }, {
+    "name": "Maggie",
+    "photo": "http://bloguin.com/awfulannouncing/wp-content/uploads/sites/94/2014/07/maggiesimpson.jpg",
+    "scores" : [5,3,2,3,2,5,1,3,3,4]
+  }];
+
+// Exporting array of variable friends
+module.exports = friends;
